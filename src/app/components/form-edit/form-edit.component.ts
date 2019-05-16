065c2d0bafb5151349c866503773e18f3bc8361e
Ezeneria <neonyyyyy@gmail.com>
2019-05-16 20:54:05 +0300

    Fix

--- src/app/components/form-edit/form-edit.component.ts
+++ src/app/components/form-edit/form-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit } from '@angular/core';
 import { EventEmmiterService } from '../../services/event-emmiter.service';
 
 
@@ -7,7 +7,8 @@ import { EventEmmiterService } from '../../services/event-emmiter.service';
   templateUrl: './form-edit.component.html',
   styleUrls: ['./form-edit.component.scss']
 })
-export class FormEditComponent implements OnInit{
+
+export class FormEditComponent implements OnInit {
   @Input() formContent;
 
   constructor(private eventEmmiterService: EventEmmiterService) {}
